ab3ced214defd5bbb7908225f350b92d6a5f8449
Bruno Staine <brunostaine@hotmail.com>
2024-04-24 16:31:06 -0300

    configs tests

--- src/main/java/br/com/brunostaine/academia/entities/Student.java
+++ src/main/java/br/com/brunostaine/academia/entities/Student.java
@@ -36,5 +36,4 @@ public class Student {
     @OneToOne(mappedBy = "student", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnore
     private Registration registration;
-
 }
--- src/main/java/br/com/brunostaine/academia/repositories/StudentRepository.java
+++ src/main/java/br/com/brunostaine/academia/repositories/StudentRepository.java
@@ -3,9 +3,6 @@ package br.com.brunostaine.academia.repositories;
 import br.com.brunostaine.academia.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
-import java.util.List;
-
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    List<Student> findByBirthDate(LocalDate newBirthDate);
+
 }
