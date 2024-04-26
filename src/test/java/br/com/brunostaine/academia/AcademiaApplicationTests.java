1776c69c61f9182e819f5926a69d54169536b333
Bruno Staine <brunostaine@hotmail.com>
2024-04-26 12:17:22 -0300

    Test main

--- src/test/java/br/com/brunostaine/academia/AcademiaApplicationTests.java
+++ src/test/java/br/com/brunostaine/academia/AcademiaApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AcademiaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		AcademiaApplication.main(new String[] {});
 	}
 
 }
